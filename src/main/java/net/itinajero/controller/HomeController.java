37551500c344bdb409c7914ad068fe25fa2a40e4
angelItza <you@example.com>
2022-09-26 23:02:00 -0500

    descuentos

--- src/main/java/net/itinajero/controller/HomeController.java
+++ src/main/java/net/itinajero/controller/HomeController.java
@@ -26,4 +26,8 @@ public class HomeController {
 	public String MostrarBase() {
 		return "/base";
 	}
+	@GetMapping("/descuento")
+	public String MostrarDescuentos() {
+		return "/descuento";
+	}
 }
