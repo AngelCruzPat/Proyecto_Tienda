75b0ec3f8cd05ebdd98b670c0551ee877de4a932
MikeCanul <mike.canuljimenez@cbtis72.edu.mx>
2022-10-23 11:06:30 -0500

    Creacion de vistas
    Commits Mio de Mike Creacion de vistas

--- src/main/java/net/itinajero/controller/HomeController.java
+++ src/main/java/net/itinajero/controller/HomeController.java
@@ -4,14 +4,18 @@ package net.itinajero.controller;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import net.itinajero.model.Productos;
+import net.itinajero.service.IProductosService;
 
 @Controller
 public class HomeController {
+	
+	
 	
 	@GetMapping("/")
 	public String MostrarHome() {
@@ -26,6 +30,12 @@ public class HomeController {
 		producto.setOferta("25%");
 		return "/detalles";
 	}
+	
+	@GetMapping("/registro")
+	public String MostrarRegistro() {
+		return "/formRegistro";
+	}
+	
 	@GetMapping("/formulario")
 	public String MostrarFormulario() {
 		return "/formulario";
@@ -42,10 +52,10 @@ public class HomeController {
 		List<Productos> lista = new LinkedList<Productos>();
 		Productos producto1 = new Productos();
 		producto1.setId(1);
-		producto1.setNombre("Chamarra");
-		producto1.setDescripcion("Chamarra de piel de dinasaurio con partes humanas");
+		producto1.setNombre("Stylish T-shart");
+		producto1.setDescripcion("dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem is simply dummy text of the printing and typesetting industry.s");
 		producto1.setImagen("no-imagen.png");
-		producto1.setPrecio("150");
+		producto1.setPrecio("$40.00");
 		producto1.setOferta("25%");
 		
 		Productos producto2 = new Productos();
@@ -67,5 +77,10 @@ public class HomeController {
 	@GetMapping("/base")
 	public String MostrarBase() {
 		return "/base";
+	}
+	
+	@GetMapping("/mensaje")
+	public String MostrarMensaje() {
+		return "/mensaje";
 	}
 }
--- src/main/java/net/itinajero/controller/ProductosController.java
+++ src/main/java/net/itinajero/controller/ProductosController.java
@@ -1,20 +1,36 @@
 package net.itinajero.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import net.itinajero.model.Productos;
 import net.itinajero.service.IProductosService;
 
-
+@Controller
 public class ProductosController {
+	
+	
 	@Autowired
 	private IProductosService serviceProductos;
-	
-	@GetMapping("/delete")
+		
+	@GetMapping("/productos")
+	public String mostrarIndex(Model model) {
+		List<Productos> lista = serviceProductos.buscarTodas();
+    	model.addAttribute("vacantes", lista);
+		return "/productos/listProductos";
+	}
+	@GetMapping("/mensaje")
 	public String eliminar(@RequestParam("id") int id, Model model) {
 		System.out.println("Borrando vacante con ID: " + id);
 		model.addAttribute("id", id);	
@@ -22,7 +38,7 @@ public class ProductosController {
 		return "mensaje";
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int id, Model model) {
 		
 		Productos producto = serviceProductos.buscarPorid(id);
@@ -34,4 +50,11 @@ public class ProductosController {
 		
 		return "detalle";
 	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
 }
